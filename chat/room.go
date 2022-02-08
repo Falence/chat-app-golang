3421ca88c7fad60f0e3f7404b23020789bbf48fa
Falence Lemungoh <falencelemungoh@gmail.com>
2022-02-08 14:18:05 +0100

    using cucurrency in room method

--- chat/room.go
+++ chat/room.go
@@ -11,3 +11,22 @@ type room struct {
 	// clients hold all current clients in this room
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <- r.join:
+			// joining
+			r.clients[client] = true
+		case client := <- r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <- r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
+}
