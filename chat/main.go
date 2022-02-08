c608f3425979e8543e4131ae0c2055ffda6c51b4
Falence Lemungoh <falencelemungoh@gmail.com>
2022-02-08 12:50:33 +0100

    separating views from logic using templates

--- chat/main.go
+++ chat/main.go
@@ -3,20 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
+
+// temple represents a single template
+type templateHandler struct {
+	once sync.Once
+	filename string
+	templ *template.Template
+}
+
+// ServeHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					Let's chat!
-				</body>
-			</html>`,
-		))
+		w.Write([]byte())
 	})
 
 	// Start the web server
