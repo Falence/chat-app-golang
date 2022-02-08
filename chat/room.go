80196aefd79542df10ebad655c84e269a4b3776f
Falence Lemungoh <falencelemungoh@gmail.com>
2022-02-08 16:25:31 +0100

    turning a room into an http handler

--- chat/room.go
+++ chat/room.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be forwarded to the other clients.
@@ -15,18 +22,45 @@ type room struct {
 func (r *room) run() {
 	for {
 		select {
-		case client := <- r.join:
+		case client := <-r.join:
 			// joining
 			r.clients[client] = true
-		case client := <- r.leave:
+		case client := <-r.leave:
 			// leaving
 			delete(r.clients, client)
 			close(client.send)
-		case msg := <- r.forward:
+		case msg := <-r.forward:
 			// forward message to all clients
 			for client := range r.clients {
 				client.send <- msg
 			}
 		}
 	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: socketBufferSize,
+}
+
+func (r *room) ServeHTTp(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send: make(chan []byte, messageBufferSize),
+		room: r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
 }
