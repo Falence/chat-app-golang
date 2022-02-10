32f323ba3b4cf5eeff888d5997c635989fbddf91
Falence Lemungoh <falencelemungoh@gmail.com>
2022-02-10 12:00:30 +0100

    implement external login logic for google

--- chat/main.go
+++ chat/main.go
@@ -4,19 +4,22 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
 	// "os"
 	"path/filepath"
 	"sync"
 	"text/template"
 
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
 	// "github.com/falence/go-blueprints/trace"
 )
 
 // temple represents a single template
 type templateHandler struct {
-	once sync.Once
+	once     sync.Once
 	filename string
-	templ *template.Template
+	templ    *template.Template
 }
 
 // ServeHTTP handles the HTTP request
@@ -30,9 +33,19 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application")
 	flag.Parse() // parse the flags
+
+	// setup gomniauth
+	gomniauth.SetSecurityKey("my-auth-key")
+	gomniauth.WithProviders(
+		google.New(
+			"310169518174-jjqlfedt8217illgaqp54o6029hljeid.apps.googleusercontent.com",
+			"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",
+			"http://localhost:8080/auth/callback/google",
+		),
+	)
+
 	r := newRoom()
 	// r.tracer = trace.New(os.Stdout)
-
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
