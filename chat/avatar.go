c56d3347004e5229f514e841f362f3b4f2abd21d
Falence Lemungoh <falencelemungoh@gmail.com>
2022-02-11 16:23:44 +0100

    abstracting the avatar url process

---
+++ chat/avatar.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"errors"
+)
+
+// ErrNoAvatar is the error that is returned when the 
+// Avatar instance is unable to provide an avatar URL.
+var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL")
+// Avatar represents types capable of representing
+// user profile pictures
+type Avatar interface {
+	// GetAvatarURL gets the avatar URL for the specified client
+	// or returns an error if something goes wrong.
+	// ErrNoAvatarURL is returned if the object is unable to get
+	// a URL for the specified client
+	GetAvatarURL(c *client) (string, error)
+}
