024ca9ef0ed92201f90e9e7da23ef8c2fdd9bb7a
Falence Lemungoh <falencelemungoh@gmail.com>
2022-02-12 11:44:12 +0100

    fixing the existing avatar implementation

--- chat/avatar.go
+++ chat/avatar.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"crypto/md5"
 	"errors"
-	"fmt"
-	"io"
 	"io/ioutil"
 	"path"
-	"strings"
 )
 
 // ErrNoAvatar is the error that is returned when the
 // Avatar instance is unable to provide an avatar URL.
 var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL")
+
 // Avatar represents types capable of representing
 // user profile pictures
 type Avatar interface {
@@ -20,11 +17,13 @@ type Avatar interface {
 	// or returns an error if something goes wrong.
 	// ErrNoAvatarURL is returned if the object is unable to get
 	// a URL for the specified client
-	GetAvatarURL(c *client) (string, error)
+	GetAvatarURL(ChatUser) (string, error)
 }
 
-type AuthAvatar struct {}
+type AuthAvatar struct{}
+
 var UseAuthAvatar AuthAvatar
+
 // func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
 // 	if url, ok := c.userData["avatar_url"]; ok {
 // 		if urlStr, ok := url.(string); ok {
@@ -33,60 +32,34 @@ var UseAuthAvatar AuthAvatar
 // 	}
 // 	return "", ErrNoAvatarURL
 // }
-func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
-	url, ok := c.userData["avatar_url"]
-	if !ok {
+func (AuthAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	url := u.AvatarURL()
+	if len(url) == 0 {
 		return "", ErrNoAvatarURL
 	}
-	urlStr, ok := url.(string)
-	if !ok {
-		return "", ErrNoAvatarURL
-	}
-	return urlStr, nil
+	return url, nil
 }
 
+type GravatarAvatar struct{}
 
-type GravatarAvatar struct {}
 var UseGravatar GravatarAvatar
-func (GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	// if email, ok := c.userData["email"]; ok {
-	// 	if emailStr, ok := email.(string); ok {
-	// 		m := md5.New()
-	// 		io.WriteString(m, strings.ToLower(emailStr))
-	// 		return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
-	// 	}
-	// }
-	// return "", ErrNoAvatarURL
-	userid, ok := c.userData["userid"]
-	if !ok {
-		return "", ErrNoAvatarURL
-	}
-	useridStr, ok := userid.(string)
-	if !ok {
-		return "", ErrNoAvatarURL
-	}
-	m := md5.New()
-	io.WriteString(m, strings.ToLower(useridStr))
-	return fmt.Sprintf("//www.gravatar.com/avatar/" + useridStr), nil
+
+func (GravatarAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	return "//www.gravatar.com/avatar/" + u.UniqueID(), nil
 }
 
+type FileSystemAvatar struct{}
 
-type FileSystemAvatar struct {}
 var UseFileSystemAvatar FileSystemAvatar
-func (FileSystemAvatar) GetAvatarURL(c *client) (string, error) {
-	if userid, ok := c.userData["userid"]; ok {
-		if useridStr, ok := userid.(string); ok {
-			files, err := ioutil.ReadDir("avatars")
-			if err != nil {
-				return "", ErrNoAvatarURL
+
+func (FileSystemAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	if files, err := ioutil.ReadDir("avatars"); err == nil {
+		for _, file := range files {
+			if file.IsDir() {
+				continue
 			}
-			for _, file := range files {
-				if file.IsDir() {
-					continue
-				}
-				if match, _ := path.Match(useridStr + "*", file.Name()); match {
-					return "/avatars/" + file.Name(), nil
-				}
+			if match, _ := path.Match(u.UniqueID()+"*", file.Name()); match {
+				return "/avatars/" + file.Name(), nil
 			}
 		}
 	}
